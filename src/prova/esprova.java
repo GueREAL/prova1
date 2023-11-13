1b2909cc5308b49151c0527bab0ebc0daad492fe
NN TOCCARE IL MIO PC <NN TOCCARE IL MIO PC@LAPTOP-385JNARS>
2023-11-13 07:34:38 +0100

    prova2

--- src/prova/esprova.java
+++ src/prova/esprova.java
@@ -3,9 +3,9 @@ package prova;
 public class esprova {
 
 	public static void main(String[] args) {
-		int n=3;
-		for(int i = 0; i<3; i++) {
-			System.out.println("Esercizio di prova");
+		int n=6;
+		for(int i = 0; i<4; i++) {
+			System.out.println("Prova di esercizio");
 		}
 	}
 
